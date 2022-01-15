ee13c070cbede5696572a291b893be476a25987a
Mateus <mateuseustaquiomm@gmail.com>
2022-01-15 19:40:47 -0300

    foii

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { TimerComponent } from './timer/timer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path': '', component: TodoComponent },
+  {'path': 'Timer', component: TimerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { TodoComponent } from './todo/todo.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
