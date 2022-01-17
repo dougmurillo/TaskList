1ce62feda5840703e3c43108acd62595c3bd9756
Mateus <mateuseustaquiomm@gmail.com>
2022-01-17 16:54:53 -0300

    foi boa

--- src/app/todo/shared/checkbox/checkbox.component.ts
+++ src/app/todo/shared/checkbox/checkbox.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss']
+  styleUrls: ['./checkbox.component.scss'],
 })
 export class CheckboxComponent implements OnInit {
+  tb_list_task = [
+    {
+      id_task: '1',
+      nm_task: 'This is my first task',
+    },
+    {
+      id_task: '2',
+      nm_task: 'This is my second task'
+    },
+    {
+      id_task: '3',
+      nm_task: 'This is my third task'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
